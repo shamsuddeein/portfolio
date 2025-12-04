d1590c8a800dd3a41d6c173254caa68b5e75d35c
shmasuddeen yusuf <dev@oxdn.localdomain>
2025-12-04 22:40:11 +0100

    correct4

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,15 @@ DEBUG = False
 
 # ALLOWED_HOSTS = ['devwithshams.pythonanywhere.com']
 # ALLOWED_HOSTS = ["portfolio-7w6b.onrender.com"]
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = [
+    "portfolio-7w6b.onrender.com",
+    "www.portfolio-7w6b.onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
+
 
 
 # Application definition
